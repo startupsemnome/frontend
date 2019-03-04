40c16b9a012272cea001b857b26da8a181b2e230
moniquemonteiro <moniquemonteiro180@gmail.com>
2019-03-04 20:45:48 -0300

    alteração no título home teste

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ export default class Home extends Component {
     return (
       <div className="container">
 
-        <h1>Home</h1>
+        <h1>Home Teste</h1>
         <HomeDash />
 
       </div>
