90c9ade761de04ff264308b20e7e08c03f5e3fe9
juliohds <tecjuliohenrique@gmail.com>
2019-03-04 18:32:56 -0300

    ADD NOT FOND

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
+// img
+import img404 from "./../images/pages/404-page-not-found.png";
+
 export default class NotFound extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div className="container">NOT A FOUND</div>;
+    return (
+      <div className="container">
+        <h1>PÁGINA NÃO ENCONTRADA</h1>
+        <img className="not-found-img" src={img404} alt="" />
+      </div>
+    );
   }
 }
