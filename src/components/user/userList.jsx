92450d921451140e83b0f5cc5d46de166edcec87
juliohds <tecjuliohenrique@gmail.com>
2019-03-06 01:07:49 -0300

    junção

--- src/components/user/userList.jsx
+++ src/components/user/userList.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import SweetAlert from "react-bootstrap-sweetalert";
+import axios from "axios";
+import env from "./../../consts";
 
 class UserList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: []
+    };
+  }
+  loadUsers() {
+    // Make a request for a user with a given ID
+    axios
+      .get(env.API + "user")
+      .then(response => {
+        // handle success
+        const data = response.data;
+        this.setState({ users: data });
+      })
+      .catch(error => {
+        // handle error
+        console.log(error + "Erro na API");
+      });
+  }
+  componentDidMount() {
+    console.log("teste");
+    this.loadUsers();
   }
   render() {
     return (
@@ -20,6 +43,34 @@ class UserList extends Component {
               Pesquisar:
             </label>
             <input type="text" className="inputFields" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Ultima Atualização</th>
+                  <th scope="col">Data Criação</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.users.map(user => {
+                  return (
+                    <tr>
+                      <td>{user.id}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.updated_at}</td>
+                      <td>{user.created_at}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@ const hostname = window && window.location && window.location.hostname;
 let tagManengerid = "GTM-5RB953Z"
 
 if (hostname === 'localhost' || hostname === '127.0.0.1') {
-    backendHost = 'http://localhost:3000/api';
+    backendHost = 'http://localhost:8000/';
     //backendHost = 'http://local.api.br/api';
 }
 
