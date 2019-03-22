966c9d04c01433e0ff9e87faeb67fcb63c02254c
sidnymolina <sidny.molina@gmail.com>
2019-03-22 14:32:44 -0300

    inicio de gráficos na dashboard

--- src/components/homeDash.jsx
+++ src/components/homeDash.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 import "./../bootstrap.min.css";
 import ReactDOM from "react-dom";
 import { Button } from "@progress/kendo-react-buttons";
 import { savePDF } from "@progress/kendo-react-pdf";
 import { Link } from "react-router-dom";
+import { Doughnut, defaults, Chart, Bar } from 'react-chartjs-2';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, } from 'recharts';
+
+import LineBarAreaComposedChart from "./graphics/lineBarAreaComposedChart.jsx";
+import StraightAnglePieChart from "./graphics/straightAnglePieChart.jsx";
+import DomainRadarChart from "./graphics/domainRadarChart.jsx";
+import ComposedResponsive from "./graphics/composedResponsive.jsx";
 
 class HomeDash extends Component {
   constructor(props) {
@@ -21,7 +28,7 @@ class HomeDash extends Component {
               <span className="percentage-number">10</span>
             </div>
             <div className="df-c">
-              <p>EMPRESAS </p>    
+              <p>EMPRESAS </p>
             </div>
           </div>
           <div className="col-sm-12 col-md-3 box">
@@ -29,7 +36,7 @@ class HomeDash extends Component {
               <span className="percentage-number">120</span>
             </div>
             <div className="df-c">
-              <p>RECURSO </p>    
+              <p>RECURSO </p>
             </div>
           </div>
           <div className="col-sm-12 col-md-3 box">
@@ -37,7 +44,7 @@ class HomeDash extends Component {
               <span className="percentage-number">65</span>
             </div>
             <div className="df-c">
-              <p>PROBLEMA </p>    
+              <p>PROBLEMA </p>
             </div>
           </div>
           <div className="col-sm-12 col-md-3 box">
@@ -45,12 +52,14 @@ class HomeDash extends Component {
               <span className="percentage-number">8</span>
             </div>
             <div className="df-c">
-              <p>USUARIO </p>    
+              <p>USUARIO </p>
             </div>
           </div>
-        </div> 
-      
+        </div>
         <h4 style={{ display: "none" }}>Dialog Shown/Hidden with Logic</h4>
+
+        <ComposedResponsive />
+
       </div>
     );
   }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import HomeDash from "./../components/homeDash";
 
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
