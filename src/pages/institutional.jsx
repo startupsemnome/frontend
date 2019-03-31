6d8276957fdc99a49b589afd2c49299631e738db
moniquemonteiro <moniquemonteiro180@gmail.com>
2019-03-31 12:41:21 -0300

    Template para Institucional

--- src/pages/institutional.jsx
+++ src/pages/institutional.jsx
@@ -1,164 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import InstitutionalForm from "../components/institutionalForm"
-import Header from "../components/Header/Header.jsx";
-import HeaderLinks from "../components/Header/HeaderLinks.jsx";
-import GridContainer from "../components/Grid/GridContainer.jsx";
-import GridItem from "../components/Grid/GridItem.jsx";
-import Parallax from "../components/Parallax/Parallax.jsx";
-import classes from "../components/components.jsx";
-import SectionDownload from "../components/Parallax/Parallax.jsx";
-import LinearProgress from '@material-ui/core/LinearProgress';
-
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import PhoneIcon from '@material-ui/icons/Phone';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HelpIcon from '@material-ui/icons/Help';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import ThumbDown from '@material-ui/icons/ThumbDown';
-import ThumbUp from '@material-ui/icons/ThumbUp';
-import Typography from '@material-ui/core/Typography';
-
-import Slideshow from "react-slidez";
-
-// imgs
-import { imgSlider1 } from "../assets/img/bg.jpg";
-
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-});
-
-class ScrollableTabsButtonForce extends React.Component {
-  state = {
-    value: 0,
-  };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color="default">
-          <Tabs
-            value={value}
-            onChange={this.handleChange}
-            variant="scrollable"
-            scrollButtons="on"
-            indicatorColor="primary"
-            textColor="primary"
-          >
-            <Tab label="Item One" icon={<PhoneIcon />} />
-            <Tab label="Item Two" icon={<FavoriteIcon />} />
-            <Tab label="Item Three" icon={<PersonPinIcon />} />
-            <Tab label="Item Four" icon={<HelpIcon />} />
-            <Tab label="Item Five" icon={<ShoppingBasket />} />
-            <Tab label="Item Six" icon={<ThumbDown />} />
-            <Tab label="Item Seven" icon={<ThumbUp />} />
-          </Tabs>
-        </AppBar>
-        {value === 0 && <TabContainer>Item One</TabContainer>}
-        {value === 1 && <TabContainer>Item Two</TabContainer>}
-        {value === 2 && <TabContainer>Item Three</TabContainer>}
-        {value === 3 && <TabContainer>Item Four</TabContainer>}
-        {value === 4 && <TabContainer>Item Five</TabContainer>}
-        {value === 5 && <TabContainer>Item Six</TabContainer>}
-        {value === 6 && <TabContainer>Item Seven</TabContainer>}
-      </div>
-    );
-  }
-}
-
-ScrollableTabsButtonForce.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
+import InstitutionalForm from "../components/institutionalForm";
 
 export default class Institutional extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      gilad: true,
-      jason: false,
-      antoine: false,
-    };
-
   }
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.checked });
-  };
 
   render() {
-    const { gilad, jason, antoine } = this.state;
-    const error = [gilad, jason, antoine].filter(v => v).length !== 2;
-
-    const { ...rest } = this.props;
     return (
-      <div>
-        <Header
-          brand="Melhores Mentes"
-          rightLinks={<HeaderLinks />}
-          fixed
-          color="transparent"
-          changeColorOnScroll={{
-            height: 400,
-            color: "white"
-          }}
-          {...rest}
-        />
-        <Parallax image={require("../assets/img/bg4.jpg")}>
-          <div className={classes.container}>
-            <GridContainer>
-              <GridItem>
-                <div className={classes.brand}>
-                  <h1 className={classes.title}></h1>
-                  <h3 className={classes.subtitle}>
-                  </h3>
-                </div>
-              </GridItem>
-            </GridContainer>
-          </div>
-        </Parallax>
-
-        <Slideshow
-
-          duration={5000}
-          showIndex
-          showArrows
-          autoplay
-          enableKeyboard
-          useDotIndex
-          slideInterval={0}
-          defaultIndex={1}
-          slides={['http://www.ufcg.edu.br/~biblioteca/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png']}
-          effect={'fade'}
-          height={'100%'}
-          width={'100%'}
-        />
+      <div className="container">
+        <h1 className="h1-main">Cadastro de Empresa</h1>
+        <InstitutionalForm />
       </div>
     );
   }
