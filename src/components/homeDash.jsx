85192abf8beb1fb97c1f980d33c90e09f04e0154
sidnymolina <sidny.molina@gmail.com>
2019-04-01 20:28:39 -0300

    inclusões do card na homedash

--- src/components/homeDash.jsx
+++ src/components/homeDash.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 import { Doughnut, defaults, Chart, Bar } from 'react-chartjs-2';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, } from 'recharts';
 import { Row, Col, Card } from 'react-materialize';
-
+import ApexCharts from 'apexcharts'
 
 import axios from "axios";
 import env from "./../consts";
@@ -15,7 +15,10 @@ import env from "./../consts";
 
 import DomainRadarChart from "./graphics/domainRadarChart.jsx";
 import ComposedResponsive from "./graphics/composedResponsive.jsx";
-import CardSimple from "./graphics/cardSimple.jsx";
+import CardSimple from "./card/cardSimple.jsx";
+import DecksExample from "./card/decksExample.jsx";
+import GroupsExample from "./card/groupsExample.jsx";
+import SizingExample from "./card/sizingExample.jsx";
 
 
 class HomeDash extends Component {
@@ -35,46 +38,81 @@ class HomeDash extends Component {
         <div className="row">
           <div className="col-sm-12 col-md-3 box">
             <div className="df-c">
-              <span className="percentage-number">{this.state.qtdCompany}</span>
-            </div>
-            <div className="df-c">
-              <p>EMPRESAS </p>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-3 box">
-            <div className="df-c">
-              <span className="percentage-number">{this.state.qtdResource}</span>
-            </div>
-            <div className="df-c">
-              <p>RECURSO </p>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-3 box">
-            <div className="df-c">
-              <span className="percentage-number">{this.state.qtdProblem}</span>
-            </div>
-            <div className="df-c">
-              <p>PROBLEMA </p>
+              <div class="card">
+                <div class="card-body">
+                  <span className="percentage-number">{this.state.qtdCompany}</span>
+                  <h5 class="card-title">Empresa</h5>
+                  <p class="card-text"> Os dados exibidos à cima são de acordo com a quanidade de empresas cadastradas.</p>
+                  <br></br>
+                  <a href="#" class="btn btn-primary">Saiba mais!</a>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Atualizado à 4 minutos atrás</small>
+                </div>
+                <div className="df-c">
+                </div>
+              </div>
             </div>
           </div>
+
           <div className="col-sm-12 col-md-3 box">
             <div className="df-c">
-              <span className="percentage-number">{this.state.qtdUsers}</span>
+              <div class="card">
+                <div class="card-body">
+                  <span className="percentage-number">{this.state.qtdResource}</span>
+                  <h5 class="card-title">Recurso</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <br></br>
+                  <a href="#" class="btn btn-primary">Saiba mais!</a>
+                </div>
+                <div className="df-c">
+                  <div class="card-footer">
+                    <small class="text-muted">Atualizado à 4 minutos atrás</small>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+
+          <div className="col-sm-12 col-md-3 box">
             <div className="df-c">
-              <p>USUARIO </p>
+              <div class="card">
+                <div class="card-body">
+                  <span className="percentage-number">{this.state.qtdProblem}</span>
+                  <h5 class="card-title">Problema</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <br></br>
+                  <a href="#" class="btn btn-primary">Saiba mais!</a>
+                </div>
+                <div className="df-c">
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Atualizado à 6 minutos atrás</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-12 col-md-3 box">
+            <div className="df-c">
+              <div class="card">
+                <div class="card-body">
+                  <span className="percentage-number">{this.state.qtdUsers}</span>
+                  <h5 class="card-title">Usuário</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <br></br>
+                  <a href="#" class="btn btn-primary">Saiba mais!</a>
+                </div>
+                <div className="df-c">
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Atualizado à 8 minutos atrás</small>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <h4 style={{ display: "none" }}>Dialog Shown/Hidden with Logic</h4>
-
-        <ComposedResponsive/>
-
-
-        <br></br>
-        <br></br>
-
-
       </div>
     );
   }
