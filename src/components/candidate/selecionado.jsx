ec98cc45304c60cf2a81d252e5bb83f3b853b5e0
Wandell <wandellcarvalho951108@gmail.com>
2019-04-22 13:35:37 -0300

    tela de bem vindo

--- src/components/candidate/selecionado.jsx
+++ src/components/candidate/selecionado.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import SweetAlert from "react-bootstrap-sweetalert";
 import { Row, Col, Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-// import { Table } from 'reactstrap';
-// import ModalLearnMore from "./modalLearnMore";
+import axios from "axios";
+import env from "./../../consts";
+import { Link } from "react-router-dom";
 class Selecionado extends Component {
   //class CompanyForm extends Component 
   constructor(props) {
@@ -11,16 +12,21 @@ class Selecionado extends Component {
     };
     console.log(this.props);
   }
+  goToProjetos() {
+    this.props.history.push("/lista-projetos");
+  }
 
   render() {
     return (
-      <div className="escolhido row">
-        <h1>Seja Bem Vindo</h1>
-        <h3>Você foi escolhido para participar do projeto <b>colocar o nome do projeto aqui de form dinamica</b></h3>
-        <p>Clique no botão para ver seu projeto :D</p>
-        <Button class="success">
+      <div className="escolhido col-md-12">
+        <div className="faixa1"></div>
+        <h1 className="titulo">Seja Bem Vindo</h1>
+        <h3 className="frase">Você foi escolhido para participar do projeto <b>colocar o nome do projeto aqui de form dinamica</b></h3>
+        <p className="continuar">Clique no botão para ver seu projeto :D</p>
+        <Button className="btn btn-success selecao" onClick={() => this.this.goToProjetos}>
           Vê projeto
         </Button>
+        <div className="faixa2"></div>
       </div>
     );
   }
