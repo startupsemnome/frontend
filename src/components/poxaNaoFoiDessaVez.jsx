fce0dd94481f12a50ef596cf35f48f8d2e8b5fe7
juliohds <tecjuliohenrique@gmail.com>
2019-05-26 17:59:10 -0300

    poxa

--- src/components/poxaNaoFoiDessaVez.jsx
+++ src/components/poxaNaoFoiDessaVez.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default class poxaNaoFoiDessaVez extends Component {
   render() {
@@ -14,13 +15,16 @@ export default class poxaNaoFoiDessaVez extends Component {
             <div style={{ backgroundColor: "#174f78", padding: 20 }}>
               <h1 className="h1-main">
                 Todos os projetos erraram o alvo dessa vez, atualize seu
-                cadastro para que os projetos consigam encontra-lo melhor.
-                <button
-                  className="join-btn-no-transform mr-1 login"
-                  onClick={this.props.history.push("/institutional#header3")}
+                cadastro para que os projetos consigam encontra-lo melhor.{" "}
+                <br />
+                <br />
+                <Link
+                  className="join-btn-no-transform mr-1 login col-md-12"
+                  to="institutional#header3"
+                  style={{ textDecoration: "none" }}
                 >
                   Atualizar Cadastro
-                </button>
+                </Link>
               </h1>
             </div>
           </div>
