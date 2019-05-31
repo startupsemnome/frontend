118f0717f2cc7f188580a10b9ced7f3136a7833f
juliohds <tecjuliohenrique@gmail.com>
2019-05-30 23:27:30 -0300

    disponibilidde

--- src/components/cadastroUsuarioCompleto.jsx
+++ src/components/cadastroUsuarioCompleto.jsx
@@ -38,6 +38,7 @@ import SweetAlert from "react-bootstrap-sweetalert";
 import axios from "axios";
 import env from "./../consts";
 
+import Disponibilidade from "./disponibilidade";
 import ComposedResponsive from "./graphics/composedResponsive.jsx";
 
 import { setNavbarOpen } from "./../redux/actions/navbarAction";
@@ -436,104 +437,7 @@ class CadastroUsuarioCompleto extends Component {
             </label>
             <br />
           </div>
-          <div className="col-md-12">
-            <div className="">
-              <table className="table table">
-                <thead>
-                  <tr>
-                    <th scope="col" style={{ display: "none" }}>
-                      ID
-                    </th>
-                    <th scope="col">Período</th>
-                    <th scope="col">Segunda-feira</th>
-                    <th scope="col">Terça-feira</th>
-                    <th scope="col">Quarta-feira</th>
-                    <th scope="col">Quinta-feira</th>
-                    <th scope="col">Sexta-feira</th>
-                    <th scope="col">Sábado</th>
-                    <th scope="col">Domingo</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Manhã</td>
-                    <td>
-                      <input name="segunda" type="checkbox" id="m-segunda" />
-                    </td>
-                    <td>
-                      <input name="terça" type="checkbox" id="m-terca" />
-                    </td>
-                    <td>
-                      <input name="quarta" type="checkbox" id="m-quarta" />
-                    </td>
-                    <td>
-                      <input name="quinta" type="checkbox" id="m-quinta" />
-                    </td>
-                    <td>
-                      <input name="sexta" type="checkbox" id="m-sexta" />
-                    </td>
-                    <td>
-                      <input name="sabado" type="checkbox" id="m-sabado" />
-                    </td>
-                    <td>
-                      <input name="domingo" type="checkbox" id="m-domingo" />
-                    </td>
-                  </tr>
-                  {/* LINHA - TARDE */}
-                  <tr>
-                    <td>Tarde</td>
-                    <td>
-                      <input name="segunda" type="checkbox" id="t-segunda" />
-                    </td>
-                    <td>
-                      <input name="terca" type="checkbox" id="t-terca" />
-                    </td>
-                    <td>
-                      <input name="quarta" type="checkbox" id="t-quarta" />
-                    </td>
-                    <td>
-                      <input name="quinta" type="checkbox" id="t-quinta" />
-                    </td>
-                    <td>
-                      <input name="sexta" type="checkbox" id="t-sexta" />
-                    </td>
-                    <td>
-                      <input name="sabado" type="checkbox" id="t-sabado" />
-                    </td>
-                    <td>
-                      <input name="domingo" type="checkbox" id="t-domingo" />
-                    </td>
-                    {/* LINHA - NOITE */}
-                  </tr>
-                  <tr>
-                    <td>Noite</td>
-                    <td>
-                      <input name="segunda" type="checkbox" id="n-segunda" />
-                    </td>
-                    <td>
-                      <input name="terca" type="checkbox" id="n-terca" />
-                    </td>
-                    <td>
-                      <input name="quarta" type="checkbox" id="n-quarta" />
-                    </td>
-                    <td>
-                      <input name="quinta" type="checkbox" id="n-quinta" />
-                    </td>
-                    <td>
-                      <input name="sexta" type="checkbox" id="n-sexta" />
-                    </td>
-                    <td>
-                      <input name="sabado" type="checkbox" id="n-sabado" />
-                    </td>
-                    <td>
-                      <input name="domingo" type="checkbox" id="n-domingo" />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-
+          <Disponibilidade />
           <div className="col-md-12">
             <label
               className="labelFields"
