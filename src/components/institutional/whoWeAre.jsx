b93bd30f09a9d2032122799a41903fc9cd8dd1b6
Monique Monteiro <monique.monteiro@nossoinstitutoanima.com.br>
2019-06-02 15:14:27 -0300

    rounded-circle no Component

--- src/components/institutional/whoWeAre.jsx
+++ src/components/institutional/whoWeAre.jsx
@@ -15,16 +15,16 @@ export default class WhoWeAre extends Component {
         <div className="row">
           <div className="col-md-10" style={{ display: "flex", margin: "auto", marginTop: "60px" }}>
               <div className="col-md-4">
-                <img style={{width: "300px", height:"300px"}} src="https://blog.softwareavaliacao.com.br/wp-content/uploads/2017/05/0089-01.jpg" alt="" srcset=""/>
+                <img className="rounded-circle" style={{width: "300px", margin:"0.5rem", border:"#0ce1f6 solid 3px", height:"300px"}} src="https://blog.softwareavaliacao.com.br/wp-content/uploads/2017/05/0089-01.jpg" alt="" srcset=""/>
                 <div><h2>Somos jovens apaixonados por tecnologia, movidos por desafios.</h2></div>
               </div>
               <div className="col-md-4">
-                <img  style={{width: "300px", height:"300px"}} src="http://diabetesevida.com.br/on/wp-content/uploads/2019/04/quem-somos.png" alt="" srcset=""/>
+                <img className="rounded-circle" style={{width: "300px", border:"#0ce1f6 solid 3px", height:"300px"}} src="http://diabetesevida.com.br/on/wp-content/uploads/2019/04/quem-somos.png" alt="" srcset=""/>
                 <div><h2>Buscamos falicitar a vida dos nossos clientes, com soluções ágeis, praticas e diretas.</h2></div>
               </div>
               <div className="col-md-4">
-                <img  style={{width: "300px", height:"300px"}} src="https://image.freepik.com/vetores-gratis/design-de-vetor-de-desenho-animado-de-trabalho-em-equipe-de-carater-empresario_1473-147.jpg" alt="" srcset=""/>
-                <div><h2>Temos a melhor solução para o seu negócio, conheça a nossa plataforma Resource Manager.</h2></div>
+                <img className="rounded-circle" style={{width: "300px", border:"#0ce1f6 solid 3px", height:"300px"}} src="https://image.freepik.com/vetores-gratis/design-de-vetor-de-desenho-animado-de-trabalho-em-equipe-de-carater-empresario_1473-147.jpg" alt="" srcset=""/>
+                <div st><h2>Temos a melhor solução para o seu negócio, conheça a nossa plataforma Resource Manager.</h2></div>
               </div>
           </div>
         </div>
