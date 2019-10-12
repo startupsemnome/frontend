52092aace45b7c5c2d4942decf533069853c31ea
sidnymolina <sidny.molina@gmail.com>
2019-10-12 15:46:17 -0300

    ajuste feed i

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -8,23 +8,59 @@ class Feed extends Component {
 
   render() {
     return (
-      <div class="ui comments">
-        <div class="comment">
-          <a class="avatar"><img src="https://react.semantic-ui.com/images/avatar/small/steve.jpg" /></a>
-          <div class="content">
-            <a class="author">Steve Jobes</a>
-            <div class="metadata"><div>2 days ago</div></div>
-            <div class="text">Revolutionary!</div>
-            <div class="actions"><a class="active">Reply</a></div>
-            <form class="ui reply form">
-              <div class="field"><textarea rows="3"></textarea></div>
-              <button class="ui icon primary left labeled button">
-                <i aria-hidden="true" class="edit icon"></i>
-                Add Reply
-        </button>
-            </form>
+      <div>
+        <div className="row">
+          <div className="col-md-12">
+            <br></br>
+            <h1 className="h1-main">Feed</h1>
           </div>
         </div>
+        <div className="container col-md-8">
+          <div
+            className="col-md-12"
+            style={{
+              backgroundColor: "#1a8687",
+              justifyContent: "center",
+              backgroundColor: "rgb(26, 134, 135)",
+              display: "flex"
+            }}
+          >
+            <input
+              type="text"
+              className="inputFields"
+              style={{ width: "100%", marginleft: "10px" }}
+              onChange={e => this.setState({ buscaTable: e.target.value })}
+            />
+            <button
+              type="button"
+              className="join-btn-no-transform mr-1 login col-md-2"
+              style={{
+                width: "100%",
+                borderRadius: "20px",
+                marginLeft: "10px"
+              }}
+              onClick={() => this.findProblem()}
+            >
+              Buscar
+          </button>
+          </div>
+        </div>
+
+        {/* <div className="row">
+          <div className="col-md-12">
+            <table className="table table-main">
+              <thead>
+                <tr>
+                  <th scope="col" style={{ display: "none" }}>
+                    ID
+                </th>
+                  <th scope="col">Empresa</th>
+
+                </tr>
+              </thead>
+            </table>
+          </div>
+        </div> */}
       </div>
     );
   }
